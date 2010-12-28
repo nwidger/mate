271735a9204941cf7c9d7e10d2cde69b525863ab
nwidger <niels.widger@gmail.com>
2010-12-28 17:30:28 +0000

    -added real.c and real.h to mvm source, added real_test.m to tests directory
    git-svn-id: svn+ssh://agate.cs.unh.edu/home/csg/nau7/svn/mate@27 8533781a-2b67-4406-8f2e-75780de0317d

---
+++ tests/real_test.m
@@ -0,0 +1,17 @@
+Integer main() {
+  Real r, s;
+
+  r = 4.5;
+
+  out r.toString() + newline;
+
+  s = 5.5;
+
+  out s.toString() + newline;
+
+  r = r + s;
+
+  out r.toString() + newline;
+
+  return 0;
+}
