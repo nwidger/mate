892cdab4e89d486cacd0807623ddb881d073a60e
nwidger <niels.widger@gmail.com>
2012-01-13 23:48:14 +0000

    Added new dmp subdir to tests dir and first dmp test, test1.m
    git-svn-id: svn+ssh://agate.cs.unh.edu/home/csg/nau7/svn/mate@92 8533781a-2b67-4406-8f2e-75780de0317d

---
+++ tests/dmp/test1.m
@@ -0,0 +1,76 @@
+class TestThread extends Thread {
+  Integer idx;
+  Table x;
+
+  TestThread(Integer idx, Table x) {
+    super();
+
+    this.idx = idx;
+    this.x = x;
+  }
+
+  Object run() {
+    this.x.put(0, this.idx);
+    return null;
+  }
+}
+
+class Main {
+  Main() {
+
+  }
+
+  Object usage() {
+    out "usage: <numThreads>" + newline;
+    return null;
+  }
+
+  Integer go() {
+    String str;
+    TestThread thread;
+    Table threads, retval;
+    Integer i, NumThreads;
+
+    // Parse arguments    
+    if ((str = in) == null) {
+      usage();
+      return 1;
+    }
+
+    NumThreads = str.toInteger();
+
+    if (NumThreads < 0) {
+      usage();
+      return 1;
+    }
+
+    // Initialize array of thread structures
+    threads = new Table(NumThreads);
+    retval = new Table(NumThreads);
+
+    for (i = 0; i < NumThreads; i = i + 1) {
+      thread = new TestThread(i, retval);
+      threads.put(i, thread);
+    }
+
+    for (i = 0; i < NumThreads; i = i + 1) {
+      thread = (TestThread)threads.get(i);
+      thread.start();
+    }
+
+    // Wait for each of the threads to terminate
+    for (i = 0; i < NumThreads; i = i + 1) {
+      thread = (TestThread)threads.get(i);
+      thread.join();
+    }
+
+    // print results
+    out "x.get(0): " + ((Integer)retval.get(0)).toString() + newline;
+
+    return 0;
+  }
+}
+
+Integer main() {
+  return new Main().go();
+}
