00351716328f46ba9a46f9967e3edcfe932fe8d3
nwidger <niels.widger@gmail.com>
2012-01-19 23:51:42 +0000

    -Added DMP-specific test, test2.m
    git-svn-id: svn+ssh://agate.cs.unh.edu/home/csg/nau7/svn/mate@94 8533781a-2b67-4406-8f2e-75780de0317d

---
+++ tests/dmp/test2.m
@@ -0,0 +1,119 @@
+class SharedObj {
+  Integer x;
+  Integer y;
+  Integer z;
+  Integer count;
+
+  SharedObj(Integer count) {
+    this.count = count;
+
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+  }
+
+  String toString() {
+    return "[ " +
+      "x = " + x.toString() + ", " +
+      "y = " + y.toString() + ", " +
+      "z = " + z.toString() +
+      " ]";
+  }
+}
+
+class TestThread extends Thread {
+  Integer idx;
+  SharedObj shared;
+
+  TestThread(Integer idx, SharedObj shared) {
+    super();
+
+    this.idx = idx;
+    this.shared = shared;
+  }
+
+  Object run() {
+    synchronized (this.shared) {
+      this.shared.count = this.shared.count - 1;
+    }
+
+    while (!this.shared.count.equals(0));
+
+    this.shared.x = idx;
+    this.shared.y = idx;
+    this.shared.z = idx;
+
+    this.sleep(50);
+
+    this.shared.x = this.shared.z * idx;
+    this.shared.y = this.shared.x * idx;
+    this.shared.z = this.shared.y * idx;
+
+    this.sleep(50);
+
+    this.shared.x = this.shared.y * idx;
+    this.shared.y = this.shared.z * idx;
+    this.shared.z = this.shared.x * idx;
+  }
+}
+
+class Main {
+  Main() {
+
+  }
+
+  Object usage() {
+    out "usage: <numThreads>" + newline;
+    return null;
+  }
+
+  Integer go() {
+    String str;
+    Table threads;
+    SharedObj shared;
+    TestThread thread;
+    Integer i, NumThreads;
+
+    // Parse arguments
+    if ((str = in) == null) {
+      usage();
+      return 1;
+    }
+
+    NumThreads = str.toInteger();
+
+    if (NumThreads < 0) {
+      usage();
+      return 1;
+    }
+
+    // Initialize array of thread structures
+    threads = new Table(NumThreads);
+    shared = new SharedObj(NumThreads);
+
+    for (i = 0; i < NumThreads; i = i + 1) {
+      thread = new TestThread(i, shared);
+      threads.put(i, thread);
+    }
+
+    for (i = 0; i < NumThreads; i = i + 1) {
+      thread = (TestThread)threads.get(i);
+      thread.start();
+    }
+
+    // Wait for each of the threads to terminate
+    for (i = 0; i < NumThreads; i = i + 1) {
+      thread = (TestThread)threads.get(i);
+      thread.join();
+    }
+
+    // print results
+    out shared.toString() + newline;
+
+    return 0;
+  }
+}
+
+Integer main() {
+  return new Main().go();
+}
